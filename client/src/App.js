89a210b7aaf0adb58ceb8672c93d3f55bdff1d3d
Jonno Katahanas <jonathan.katahanas@outlook.com>
2021-04-12 22:29:04 +1000

    Turned on props for the majority of editor nodes.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { NotesContainer } from "./pages/NotesContainer";
 export const App = () => {
   return (
     <Router>
-      <div>
+      <div h="100vh" w="100vw">
         <NotesContainer />
       </div>
     </Router>
--- client/src/components/EditorContainer.js
+++ client/src/components/EditorContainer.js
@@ -1,12 +1,49 @@
 import React from "react";
 import { Editor } from "@atlaskit/editor-core";
+import PageHeader from "@atlaskit/page-header";
+import styled from "styled-components";
 
 const EditorContainer = (props) => {
   // const [editorState, setEditorState] = useState();
 
   return (
     <div>
-      <Editor />
+      <PageHeader>THIS WILL BE A TITLE INPUT</PageHeader>
+      <Editor
+        appearance="full-page"
+        allowFindReplace={true}
+        allowExpand={true}
+        allowExpand={{
+          allowInsertion: true,
+          allowInteractiveExpand: true,
+        }}
+        placeholder="G'day! Type away :)"
+        placeholderHints="Type / to insert content"
+        allowDate={true}
+        allowKeyboardAccessibleDatepicker={true}
+        allowStatus={true}
+        allowLayouts={true}
+        allowPanel={true}
+        allowBlockType={true}
+        allowBreakout={true}
+        allowTextColor={true}
+        allowTables={true}
+        allowTables={{
+          advanced: true,
+          allowBackgroundColor: true,
+          allowHeaderColumn: true,
+          allowHeaderRow: true,
+          allowMergeCells: true,
+          allowNumberColumn: true,
+          allowColumnSorting: true,
+          stickToolbarToBottom: true,
+          tableCellOptimization: true,
+          stickyHeaders: true,
+          stickyHeadersOptimization: true,
+          initialRenderOptimization: true,
+          mouseMoveOptimization: true,
+        }}
+      />
     </div>
   );
 };
